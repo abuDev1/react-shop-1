5f2f6ecaf015c5f1ff672921437b6c6d32087db7
lincode <burrouws@bk.ru>
2024-09-08 17:24:15 +0300

    modify components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,12 +7,12 @@ import { Button } from './Button'
 export const Card = ({product}) => {
   return (
     <div className="card" >
-      <div class="card-image">
+      <div className="card-image">
         <img src={product.image} alt="X" />
       </div>
-      <div class="card-info">
-        <div class="card-name">{product.name}</div>
-        <div class="card-price">{product.price}</div>
+      <div className="card-info">
+        <div className="card-name">{product.name}</div>
+        <div className="card-price">{product.price}</div>
         <Button product={product}/>
         </div>
     </div>
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,7 +7,7 @@ export const Content = ({products}) => {
  
   return (
     <div className='content'>
-    {products.map((product) => <Card product={product}/>)}
+    {products.map((product) => <Card product={product} key={product.id} />)}
     </div>
   )
 }
